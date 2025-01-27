26d353bc9f042aa20cd462be750487a76e3d80d9
Phoenix <68404504+PhoenixStroh@users.noreply.github.com>
2025-01-27 12:56:01 -0500

    modified starting worlds

--- Script/Level.gd
+++ Script/Level.gd
@@ -19,59 +19,59 @@ var SceneExplo = load("res://Scene/Explosion.tscn")
 var check := false
 
 func _ready():
-	if level_type != LevelType.NORMAL:
-		var p = ScenePlayer.instantiate()
-		p.position = Vector2(72, 85)
-		p.scale.x = -1 if randf() < 0.5 else 1
-		p.set_script(null)
-		add_child(p)
+    if level_type != LevelType.NORMAL:
+        var p = ScenePlayer.instantiate()
+        p.position = Vector2(72, 85)
+        p.scale.x = -1 if randf() < 0.5 else 1
+        p.set_script(null)
+        add_child(p)
 
-	MapStart()
+    MapStart()
 
 func MapStart():
-	for pos in Map.get_used_cells():
-		var id = Map.get_cell_source_id(pos)
-		match id:
-			TILE_WALL:
-				# Use random wall tile from 3×3 tileset to make levels look less repetitive
-				var atlas = Vector2(randi_range(0, 2), randi_range(0, 2))
-				Map.set_cell(pos, TILE_WALL, atlas)
-			TILE_PLAYER:
-				# Add live player to the scene
-				var inst = ScenePlayer.instantiate()
-				inst.position = Map.map_to_local(pos) + Vector2(4, 0)
-				self.add_child(inst)
-				inst.died.connect(_on_died.bind(inst))
-				inst.stomped.connect(_on_stomped)
-				# Remove static player tile from the tile map
-				Map.set_cell(pos, -1)
-			TILE_GOOBER:
-				# Add live goober to the scene
-				var inst = SceneGoober.instantiate()
-				inst.position = Map.map_to_local(pos) + Vector2(4, 0)
-				NodeGoobers.add_child(inst)
-				# Remove static goober tile from the tile map
-				Map.set_cell(pos, -1)
+    for pos in Map.get_used_cells():
+        var id = Map.get_cell_source_id(pos)
+        match id:
+            TILE_WALL:
+                # Use random wall tile from 3×3 tileset to make levels look less repetitive
+                var atlas = Vector2(randi_range(0, 2), randi_range(0, 2))
+                Map.set_cell(pos, TILE_WALL, atlas)
+            TILE_PLAYER:
+                # Add live player to the scene
+                var inst = ScenePlayer.instantiate()
+                inst.position = Map.map_to_local(pos) + Vector2(4, 0)
+                self.add_child(inst)
+                inst.died.connect(_on_died.bind(inst))
+                inst.stomped.connect(_on_stomped)
+                # Remove static player tile from the tile map
+                Map.set_cell(pos, -1)
+            TILE_GOOBER:
+                # Add live goober to the scene
+                var inst = SceneGoober.instantiate()
+                inst.position = Map.map_to_local(pos) + Vector2(4, 0)
+                NodeGoobers.add_child(inst)
+                # Remove static goober tile from the tile map
+                Map.set_cell(pos, -1)
 
 func _process(_delta: float):
-	# should i check?
-	if check:
-		check = false
-		var count = NodeGoobers.get_child_count()
-		print("Goobers: ", count)
-		if count == 0:
-			win.emit()
+    # should i check?
+    if check:
+        check = false
+        var count = NodeGoobers.get_child_count()
+        print("Goobers: ", count)
+        if count == 0:
+            win.emit()
 
 func Explode(character: Node2D):
-	var xpl = SceneExplo.instantiate()
-	xpl.position = character.position
-	add_child(xpl)
-	character.queue_free()
+    var xpl = SceneExplo.instantiate()
+    xpl.position = character.position
+    add_child(xpl)
+    character.queue_free()
 
 func _on_died(player: CharacterBody2D):
-	Explode(player)
-	lose.emit()
+    Explode(player)
+    lose.emit()
 
 func _on_stomped(goober: CharacterBody2D):
-	Explode(goober)
-	check = true
+    Explode(goober)
+    check = true
--- Script/World.gd
+++ Script/World.gd
@@ -24,81 +24,81 @@ const CHANGE_DELAY := 1.5
 ## TODO: in Godot 4.4, use ResourceLoader.list_directory()
 ##  https://docs.godotengine.org/en/latest/classes/class_resourceloader.html#class-resourceloader-method-list-directory
 func _list_scenes(path: String) -> Array[String]:
-	var scenes: Array[String]
-	var dir := DirAccess.open(path)
+    var scenes: Array[String]
+    var dir := DirAccess.open(path)
 
-	dir.list_dir_begin()
-	var file := dir.get_next()
-	while file:
-		if file.ends_with(".tscn.remap"):
-			scenes.append(file.left(-len(".remap")))
-		elif file.ends_with(".tscn"):
-			scenes.append(file)
-		file = dir.get_next()
-	dir.list_dir_end()
+    dir.list_dir_begin()
+    var file := dir.get_next()
+    while file:
+        if file.ends_with(".tscn.remap"):
+            scenes.append(file.left(-len(".remap")))
+        elif file.ends_with(".tscn"):
+            scenes.append(file)
+        file = dir.get_next()
+    dir.list_dir_end()
 
-	return scenes
+    return scenes
 
 ## Load a series of numbered scenes from the given directory.
 ## Each must be a TileMapLayer node.
 func _load_levels(level_directory: String):
-	var level_regexp := RegEx.create_from_string("^\\d+.*\\.tscn$")
-	var scenes := _list_scenes(level_directory).filter(func (filename: String): return level_regexp.search(filename))
-	scenes.sort_custom(func (a: String, b: String): return a.naturalnocasecmp_to(b) < 0)
+    var level_regexp := RegEx.create_from_string("^\\d+.*\\.tscn$")
+    var scenes := _list_scenes(level_directory).filter(func (filename: String): return level_regexp.search(filename))
+    scenes.sort_custom(func (a: String, b: String): return a.naturalnocasecmp_to(b) < 0)
 
-	for scene_filename in scenes:
-		var l := load(level_directory.path_join(scene_filename)) as PackedScene
-		levels.append(l)
+    for scene_filename in scenes:
+        var l := load(level_directory.path_join(scene_filename)) as PackedScene
+        levels.append(l)
 
-	lastLevel = levels.size() - 1
+    lastLevel = levels.size() - 1
 
 func _ready() -> void:
-	_load_levels(self.scene_file_path.get_base_dir())
-	_instantiate_level()
+    _load_levels(self.scene_file_path.get_base_dir())
+    _instantiate_level()
 
 func _instantiate_level():
-	if _level_scene:
-		remove_child(_level_scene)
-		_level_scene.queue_free()
+    if _level_scene:
+        remove_child(_level_scene)
+        _level_scene.queue_free()
 
-	_level_scene = levels[level].instantiate()
-	_level_scene.win.connect(_on_win)
-	_level_scene.lose.connect(_on_lose)
-	_candy_spawner.add_sibling(_level_scene)
+    _level_scene = levels[level].instantiate()
+    _level_scene.win.connect(_on_win)
+    _level_scene.lose.connect(_on_lose)
+    _candy_spawner.add_sibling(_level_scene)
 
-	if level == firstLevel:
-		NodeOverlay.visible = true
-		NodeOverlay.frame = 0
-	elif level == lastLevel:
-		NodeOverlay.visible = true
-		NodeOverlay.frame = 3
-	else:
-		NodeOverlay.visible = false
+    if level == firstLevel:
+        NodeOverlay.visible = true
+        NodeOverlay.frame = 0
+    elif level == lastLevel:
+        NodeOverlay.visible = true
+        NodeOverlay.frame = 3
+    else:
+        NodeOverlay.visible = false
 
-	_candy_spawner.progress = float(level - firstLevel) / (lastLevel - firstLevel)
+    _candy_spawner.progress = float(level - firstLevel) / (lastLevel - firstLevel)
 
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_cancel"):
-		get_tree().change_scene_to_file("res://Scene/WorldSelector.tscn")
+    if event.is_action_pressed("ui_cancel"):
+        get_tree().change_scene_to_file("res://Scene/WorldSelector.tscn")
 
 func _process(_delta: float):
-	if Input.is_action_just_pressed("jump") and (level == firstLevel or level == lastLevel):
-		level = posmod(level + 1, levels.size())
-		_instantiate_level()
+    if Input.is_action_just_pressed("jump") and (level == firstLevel or level == lastLevel):
+        level = posmod(level + 1, levels.size())
+        _instantiate_level()
 
 func _on_win():
-	level = posmod(level + 1, levels.size())
-	NodeAudioWin.play()
-	NodeOverlay.visible = true
-	NodeOverlay.frame = 1
-	await get_tree().create_timer(CHANGE_DELAY).timeout
-	_instantiate_level()
+    level = posmod(level + 1, levels.size())
+    NodeAudioWin.play()
+    NodeOverlay.visible = true
+    NodeOverlay.frame = 1
+    await get_tree().create_timer(CHANGE_DELAY).timeout
+    _instantiate_level()
 
 func _on_lose():
-	level = max(level - 1, firstLevel)
-	NodeAudioLose.play()
-	NodeOverlay.visible = true
-	NodeOverlay.frame = 2
-	await get_tree().create_timer(CHANGE_DELAY).timeout
-	_instantiate_level()
+    level = max(level - 1, firstLevel)
+    NodeAudioLose.play()
+    NodeOverlay.visible = true
+    NodeOverlay.frame = 2
+    await get_tree().create_timer(CHANGE_DELAY).timeout
+    _instantiate_level()
