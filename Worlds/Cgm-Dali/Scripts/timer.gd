163be4e1f5ce36f928cbb65c4891e0b14675da44
Will Thompson <wjt@endlessos.org>
2025-02-07 12:41:07 +0000

    Cgm-Dali: Stop timers when player wins or dies
    Previously, the timer would continue running after the player has won
    the level or died.
    
    In the case where the player has died, this was harmless: the timer
    would keep running to zero, but noting bad would happen.
    
    In the case where the player has won the level, this was problematic: if
    the player won the level with less than 1.5 seconds remaining, then
    while the “level complete” message was showing, the player would die
    when the timer expired and have to attempt the same level again. This
    was frustrating and, at least for me, made the final level impossible.
    
    In the timer, monitor for the case when the player has won or died; when
    this happens, stop and hide the timer.
    
    This could be made cleaner if Level.gd knew about the concept of the
    level having a time limit: if that were the case, the level could just
    stop the timer, rather than having to repeat the conditions here. But it
    is a relatively small amount of code to do it here.

--- Worlds/Cgm-Dali/Scripts/timer.gd
+++ Worlds/Cgm-Dali/Scripts/timer.gd
@@ -7,6 +7,7 @@ extends Control
 
 @onready var timer: Timer = $Timer
 @onready var warning_timer: Timer = $WarningTimer
+var player: Player
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,18 +19,38 @@ func _ready() -> void:
 	warning_timer.timeout.connect(_anim_play)
 	warning_timer.start()
 
+	# Hack: we can't find the player here in _ready() because TileMapLayer only
+	# instantiates its child scenes at the end of the current frame.
+	_find_player.call_deferred()
+
+
+func _find_player() -> void:
+	player = get_tree().get_first_node_in_group("player")
+	if player:
+		player.died.connect(_stop)
+
+
+func _stop():
+	timer.stop()
+	warning_timer.stop()
+	anim_player.stop()
+	hide()
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	label.text = str(ceil(timer.time_left))
 
+	if get_tree().get_node_count_in_group("goober") == 0:
+		_stop()
+
+
 # player died when running out
 func _time_out():
 	anim_player.stop()
-
-	var player = get_tree().get_first_node_in_group("player")
-	if (player):
+	if player:
 		player.died.emit()
-	
+
+
 func _anim_play():
 	anim_player.play("low_on_time")
