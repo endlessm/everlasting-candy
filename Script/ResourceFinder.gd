0067367383b35018e07fa55aab10e464c99bbf0f
Will Thompson <wjt@endlessos.org>
2025-03-10 11:49:07 +0000

    Use ResourceLoader.list_directory() to find levels
    This new function, added in Godot 4.4, avoids the issue that resources
    (like scenes) must be loaded by their original path, while in the
    exported game DirAccess yields the real filenames used when the resource
    has been converted during export.
    
    Remove the ResourceFinder helper function entirely, and use
    ResourceLoader.list_directory() directly at its one call site.
    Convert the returned PackedStringArray to a plain Array to be able to
    use .filter() and .sort_custom().

--- Script/ResourceFinder.gd
+++ Script/ResourceFinder.gd
@@ -8,31 +8,6 @@ class_name ResourceFinder
 const _IMAGE_EXTENSIONS = [".png", ".jpg", ".jpeg", ".svg", ".webp"]
 
 
-## Lists scenes in the given resource directory, accounting for the fact that,
-## when exported, resource files are renamed, but must be loaded by their
-## original name.
-##
-## Returns the original basename of each scene in the given directory.
-##
-## TODO: in Godot 4.4, use ResourceLoader.list_directory()
-##  https://docs.godotengine.org/en/latest/classes/class_resourceloader.html#class-resourceloader-method-list-directory
-static func list_scenes(path: String) -> Array[String]:
-	var scenes: Array[String]
-	var dir := DirAccess.open(path)
-
-	dir.list_dir_begin()
-	var file := dir.get_next()
-	while file:
-		if file.ends_with(".tscn.remap"):
-			scenes.append(file.left(-len(".remap")))
-		elif file.ends_with(".tscn"):
-			scenes.append(file)
-		file = dir.get_next()
-	dir.list_dir_end()
-
-	return scenes
-
-
 ## Lists images in the given resource directory, recursively.
 ##
 ## Returns the absolute path of each image, or an empty list if the directory
